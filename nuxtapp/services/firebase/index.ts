5c3e29256cadef7f60f5363485f861d024511d2b
nikolav <admin@nikolav.rs>
2023-01-07 05:59:20 +0000

    Sat Jan  7 06:59:19 CET 2023

--- nuxtapp/services/firebase/index.ts
+++ nuxtapp/services/firebase/index.ts
@@ -1,24 +1,24 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
-import { getDatabase } from "firebase/database";
+// import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 // import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
+// import { getStorage } from "firebase/storage";
 import config from "./config";
 
 // @init
 const app = 0 < getApps().length ? getApp() : initializeApp(config);
 // const auth = getAuth(app);
 const db = getFirestore(app);
-const dbRealtime = getDatabase(app);
-const storage = getStorage(app);
+// const dbRealtime = getDatabase(app);
+// const storage = getStorage(app);
 
 const client = {
   app,
   config,
   // auth,
   db,
-  dbRealtime,
-  storage,
+  // dbRealtime,
+  // storage,
 };
 
 export default client;
