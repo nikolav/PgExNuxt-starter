7fb01a300505c9529b05b32a49b11efedf83c431
nikolav <admin@nikolav.rs>
2023-03-14 20:51:47 +0100

    Tue Mar 14 20:51:41 CET 2023

--- nuxtapp/components/ui/TeleportEnd/TeleportEnd.vue
+++ nuxtapp/components/ui/TeleportEnd/TeleportEnd.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 const { OVERLAYS_ID_END } = useAppConfig();
-const toEnd = computed(() => `#${OVERLAYS_ID_END}`);
+// const toEnd = computed(() => `#${OVERLAYS_ID_END}`);
+const toEnd = `#${OVERLAYS_ID_END}`;
 </script>
 
 <template>
--- nuxtapp/components/ui/TeleportStart/TeleportStart.vue
+++ nuxtapp/components/ui/TeleportStart/TeleportStart.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 const { OVERLAYS_ID_START } = useAppConfig();
-const toStart = computed(() => `#${OVERLAYS_ID_START}`);
+// const toStart = computed(() => `#${OVERLAYS_ID_START}`);
+const toStart = `#${OVERLAYS_ID_START}`;
 </script>
 
 <template>
