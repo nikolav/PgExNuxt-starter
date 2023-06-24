a763c8f8c04b0c24972fc3432f3b3ed72110fe4b
nikolav <admin@nikolav.rs>
2023-06-24 17:37:11 +0100

    + nikolav-treets

--- nuxtapp/pages/demo.vue
+++ nuxtapp/pages/demo.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import { A } from "nikolav-treets";
 import demoGallerySlides from "@/assets/gallery--test.json";
 
 const {
   $lightbox: { open: openGallery },
 } = useNuxtApp();
 const gallery = () => openGallery(demoGallerySlides);
+const res = JSON.stringify({ a: new A() });
 
 // #eos
 </script>
@@ -12,6 +14,11 @@ const gallery = () => openGallery(demoGallerySlides);
 <template>
   <VContainer fluid>
     <h1 class="text-h1">@demo</h1>
+    <VSheet class="ma-2 pa-4" max-width="550px">
+      <p>
+        {{ res }}
+      </p>
+    </VSheet>
     <VBtn @click="gallery">
       <VIcon start icon="$iconredeye" size="18" />
       ok.gallery
