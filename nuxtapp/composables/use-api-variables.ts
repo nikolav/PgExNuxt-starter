54dbbf7a2006c800f1f7757dccdf8dd5acb960df
nikolav <admin@nikolav.rs>
2023-07-05 02:05:35 +0100

    Wed Jul  5 03:04:56 CEST 2023

--- nuxtapp/composables/use-api-variables.ts
+++ nuxtapp/composables/use-api-variables.ts
@@ -1,21 +1,45 @@
 import { useStoreAuth } from "@/store";
 import { TPrimitive, TVariableValue, IVariable } from "@/types";
+import { get, find, reduce, findIndex } from "@/utils";
 // @@
 export const useApiVariables = () => {
   const { URL_VARIABLES, IOEVENT_VARIABLES_CHANGE } = useAppConfig();
 
   const auth = useStoreAuth();
   const AT = computed(() => auth.token?.accessToken || "");
+  const headers = computed(() => ({
+    Authorization: `Bearer ${AT.value}`,
+  }));
 
-  const { data: ls, refresh } = useLazyAsyncData<IVariable[]>(
-    "variables@api",
-    () =>
-      $fetch(URL_VARIABLES, {
-        headers: {
-          Authorization: `Bearer ${AT.value}`,
-        },
-      })
+  const { data: ls, refresh } = useFetch<IVariable[]>(URL_VARIABLES, {
+    key: "variables@api",
+    lazy: true,
+    headers,
+    default: () => [],
+  });
+
+  const lsNamed = computed(() =>
+    reduce(
+      ls.value,
+      (res: Record<string, IVariable>, node) => {
+        res[node.name] = node;
+        return res;
+      },
+      {}
+    )
   );
+
+  const byName = (varName: string) =>
+    computed(() =>
+      get(
+        find(ls.value, (node) => varName === node.name),
+        "value"
+      )
+    );
+
+  const exists = (varName: string) =>
+    computed(() => -1 < findIndex(ls.value, (node) => varName === node.name));
+
   const reloadVariables = async () => await refresh();
 
   const { $socket } = useNuxtApp();
@@ -33,9 +57,7 @@ export const useApiVariables = () => {
         name,
         value,
       },
-      headers: {
-        Authorization: `Bearer ${AT.value}`,
-      },
+      headers,
     });
   };
 
@@ -43,14 +65,18 @@ export const useApiVariables = () => {
     await $fetch(URL_VARIABLES, {
       method: "delete",
       body: { id },
-      headers: {
-        Authorization: `Bearer ${AT.value}`,
-      },
+      headers,
     });
   };
   //
   return {
+    // @read
     ls,
+    lsNamed,
+    byName,
+    exists,
+    reload: reloadVariables,
+    // @write
     put,
     drop,
   };
--- nuxtapp/pages/index.vue
+++ nuxtapp/pages/index.vue
@@ -8,12 +8,16 @@ useHead({
 
 const { name: screenSizeName } = useDisplay();
 
+const { byName: getVarByName } = useApiVariables();
+const adminName$ = getVarByName("admin:name");
+const adminEmail$ = getVarByName("admin:email");
 // @@
 </script>
 
 <template>
   <VContainer class="p-2 min-h-[256px]">
     <h2 class="text-h4">Screen size: [{{ screenSizeName }}]</h2>
+    <h2 class="text-h4">{{ adminName$ }} @ [{{ adminEmail$ }}]</h2>
     <VContainer fluid>
       <VRow>
         <VCol class="text-center">
