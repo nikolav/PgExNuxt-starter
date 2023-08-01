a48c964cea935886008453a89af17a89216299a4
nikolav <admin@nikolav.rs>
2023-08-01 03:22:51 +0100

    Tue Aug  1 04:22:50 CEST 2023

--- nuxtapp/plugins/03.add-hooks.ts
+++ nuxtapp/plugins/03.add-hooks.ts
@@ -1,10 +1,15 @@
 import { False } from "@/utils";
 
+// #https://nuxt.com/docs/api/advanced/hooks#lifecycle-hooks
 export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.hook("app:created", () => {
+    const { $ISAUTH } = useAppConfig();
+    useState($ISAUTH, False);
+  });
+
   // https://nuxt.com/docs/api/composables/use-nuxt-app#hookname-cb
   nuxtApp.hook("app:mounted", () => {
-    const { $ISMOUNTED, $ISAUTH } = useAppConfig();
-    useState($ISAUTH, False);
+    const { $ISMOUNTED } = useAppConfig();
     useState($ISMOUNTED).value = true;
   });
 });
